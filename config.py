a4e4cb62ed2beaf2382de63be83bdf6466907338
iamgabbar168 <githubheruko@gmail.com>
2026-04-20 15:33:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", "20567114"))
-    API_HASH = environ.get("API_HASH", "8a5b92106e45fc6637a65a67df060a65")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8484544285:AAEmtMxFCHYCx15Um8_Q1VfiiIESdk2o4EU") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ifsabhi64:ifsabhi64@cluster0.hgnvwqi.mongodb.net/?appName=Cluster0")
+    API_ID = int(environ.get("API_ID", "26184715"))
+    API_HASH = environ.get("API_HASH", "7fc42fe25c89660b4e0b00dd7aa0beb1")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8225077803:AAGqTGGMpLo4fywYcRDASFD4RhUDp2NrPl0") 
+    BOT_SESSION = environ.get("BOT_SESSION", "GABBAR") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dubeyji0995:Gqaqduzpm98kT6ZF@cluster0.ekxizvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "8036182138"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7744686564"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
